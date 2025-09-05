f2b3467daa1ffdbb2db6a73a241d8e792db97b0b
Mateo-de-la-Torre <mateo.delatorre@gmail.com>
2025-09-05 20:38:57 -0300

    estructura de carpetas

--- src/main.js
+++ src/main.js
@@ -1,11 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
-
-dotenv.config({ quiet: true });
+import { mainRouter } from "./routes/index.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT);
 
-console.log(`Server listening on port ${PORT}`);
+// Middleware
+app.use(express.json());
+dotenv.config({ quiet: true });
+
+app.use("/api", mainRouter);
+
+
+async function main() {
+    try {
+        app.listen(PORT);
+        console.log(`Server listening on port ${PORT}`);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+main();
---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import authRouter from "./auth.routes.js";
+
+
+
+export const mainRouter = Router();
+
+
+mainRouter.use("/auth", authRouter);
